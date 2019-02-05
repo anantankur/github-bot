2fa286304ed72f0caec5727f434184dbf2ac1872
Matt Smith <mrmcchow@gmail.com>
2019-02-05 15:22:18 +0000

    Work in progress check if auto merge should happen

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('./configs/autoMerge')
 module.exports = app => {
 
   app.log('The Rusty Git loaded successfully!');
@@ -38,17 +39,7 @@ const sendInvite = (context, app) => {
 }
 
 // Checks if auto merge should take place
-const shouldMerge = async (context, app) => {
-  const files = await context.github.pullRequests.listFiles(context.issue())
-  const data = files.data[0]
-  console.log(data.filename, data.status, data.additions, data.deletions, data.changes)
 
-  if(data.filename === "readme.md") mergePull(context, app)
-  if(data.filename === "contributors.md" && data.additions < 5 && data.deletions < 3) mergePull(context, app)
-
-  const issueComment = context.issue({ body: 'Thanks for opening this pull request!' });
-  return context.github.issues.createComment(issueComment);
-}
 
 // Merges PR if check is successfull
 const mergePull = (context, app) => {
